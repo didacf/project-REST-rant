0371ccc29b799fe0b926a050b9369a98dc736b61
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-06 00:16:43 -0700

    Final Rest Rant 3

--- controllers/places.js
+++ controllers/places.js
@@ -1,12 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
-
-// GET /places
-app.get('/', (req, res) => {
-    res.render('places/index', { places })
+    
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
@@ -19,7 +14,10 @@ app.get('/', (req, res) => {
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: 'http://placekitten.com/250/250'
-      }]  
-  })
-  
+      }]
+      
+      res.render('places/index', { places })   
+})
+ 
+
 module.exports = router
