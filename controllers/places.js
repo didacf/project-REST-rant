2a5fb0478c11254d5d4f2644af9b3d548bd50045
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-06 11:46:19 -0800

    Delete functionality Implemented

--- controllers/places.js
+++ controllers/places.js
@@ -37,7 +37,8 @@ router.get('/:id', (req, res) => {
       res.render('error404')
     }
     else {
-      res.render('places/show', { place: places[id] })
+      places.splice(id, 1)
+      res.redirect('/places')
     }
 })
   
