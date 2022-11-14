d0046ad0fa9ffd32eee40c56c41ea9c3002d2731
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-13 18:03:22 -0800

    Lesson 7 Mongoose and Schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,11 +5,12 @@ function show (data) {
     return (
         <Def>
           <main>
-          <h1>{data.place.name}</h1>
-            <div>
+          <div className='container'>
+              <div class="row">
+                <div class="col-sm-6 mt-3">     
               <img src={data.place.pic} alt={data.place.name} />
             </div>
-            <div>
+            <div class="col-sm-6 mt-3">
                 <h1>{ data.place.name }</h1>
                 <h2>City: { data.place.city}</h2>
                 <h2>State: { data.place.state}</h2>
@@ -18,14 +19,24 @@ function show (data) {
                 <p>Not Rated</p>
                 <h2>Description</h2>
                 <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines} </p>
+                <div className="btn-container pt-3">
+                    <div className='d-flex flex-column'>
+                      <div className='col pb-2'>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                     Edit
-                </a>      
+                </a>
+                </div>
+                <div className='col'>     
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
                      Delete
                     </button>
                 </form>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
             </div>
             <hr/>
             <footer>
