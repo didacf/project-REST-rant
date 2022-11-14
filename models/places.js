003b20fc837173b0fb00bd108345235ab192d0b1
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-11-14 05:47:04 -0800

    Seeding data, validations, and helper methods

--- models/places.js
+++ models/places.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const placeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     pic: { type: String, default: '/images/salad.jpg' },
